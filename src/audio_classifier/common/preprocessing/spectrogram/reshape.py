1c2d922fbdcd431edad7f2492771cb54a38f5fc7
busFred <32878707+busFred@users.noreply.github.com>
2021-09-25 14:06:07 -0400

    add unflatten_slice

--- src/audio_classifier/common/preprocessing/spectrogram/reshape.py
+++ src/audio_classifier/common/preprocessing/spectrogram/reshape.py
@@ -9,12 +9,13 @@ def slice_spectrogram(spectrogram: np.ndarray,
     """Slice a spectrogram.
 
     Args:
-        spectrogram (np.ndarray): (n_sample_freq, n_sample_time) raw spectrogram to be slice.
-        shingle_size (int): number of time bins for each sliced spectrogram
-        stride_size (int): number of time bins to skip while slicing.
+        spectrogram (np.ndarray): (n_sample_freq, n_sample_time) Raw spectrogram to be slice.
+        slice_size (int): Number of time bins for each sliced spectrogram
+        stride_size (int): Number of time bins to skip while slicing.
+        copy (bool): if `True`, then the returned slices has no relation with the passed in spectrogram. Defaults to `False`.
 
     Returns:
-        slices (np.ndarray): (n_slices, n_sample_freq, shingle_size) sliced spectrogram for the given raw_spectrogram.
+        slices (np.ndarray): (n_slices, n_sample_freq, slice_size) Sliced spectrogram for the given raw_spectrogram.
     """
     slices: List[np.ndarray] = list()
     for i in range(0, spectrogram.shape[1] - slice_size + 1, stride_size):
@@ -30,15 +31,37 @@ def flatten_slice(slice: np.ndarray, copy: bool = False) -> np.ndarray:
     """Convert a slice of spectrogram in to a row vector.
 
     Args:
-        slice (np.ndarray): (n_sample_freq, shingle_size)
+        slice (np.ndarray): (n_sample_freq, slice_size) The spectrogram slice to be flatten.
+        copy (bool): If `True`, then the returned `flat_slice` has no relation with the passed in `slice`. Defaults to `False`.
 
     Returns:
-        flat_slice (np.ndarray): (n_sample_freq * shingle_size) `[slice[:, 0].T, slice[:, 1].T, ..., slice[:, shingle_size-1].T]`
+        flat_slice (np.ndarray): (n_sample_freq * slice_size) A flattened spectrogram slice in the order of `[slice[:, 0].T, slice[:, 1].T, ..., slice[:, slice_size-1].T]`
     """
     n_sample_freq: int = slice.shape[0]
-    shingle_size: int = slice.shape[1]
-    flat_slice: np.ndarray = slice.reshape((n_sample_freq * shingle_size),
+    slice_size: int = slice.shape[1]
+    flat_slice: np.ndarray = slice.reshape((n_sample_freq * slice_size),
                                            order="F")
     if copy == True:
         return np.copy(flat_slice)
     return flat_slice
+
+
+def unflatten_slice(flat_slice: np.ndarray,
+                    slice_size: int,
+                    copy: bool = False):
+    """Unflatten a slice back to original spectrogram shape.
+
+    Args:
+        flat_slice (np.ndarray): (n_sample_freq*slice_size) A flattened spectrogram slice in the order of `[*(slice[:, 0].T), *(slice[:, 1].T), ..., *(slice[:, slice_size-1].T)]` to be restored.
+        slice_size (int): Number of time bins for each sliced spectrogram.
+        copy (bool, optional): If `True`, then the returned `spec_slice` has no relaition with the passed in `flat_slice`. Defaults to False.
+
+    Returns:
+        spec_slice (np.ndarray): (n_sample_freq, slice_size) The restored sliced spectrogram.
+    """
+    n_sample_freq: int = flat_slice.shape[0] // slice_size
+    spec_slice: np.ndarray = flat_slice.reshape((n_sample_freq, slice_size),
+                                                order="F")
+    if copy == True:
+        return np.copy(spec_slice)
+    return spec_slice
