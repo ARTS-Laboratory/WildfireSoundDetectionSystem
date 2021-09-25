0b0fe108f275060fb9681348b17e883072987418
busFred <32878707+busFred@users.noreply.github.com>
2021-09-25 13:40:42 -0400

    add copy argument

--- src/audio_classifier/common/preprocessing/spectrogram/reshape.py
+++ src/audio_classifier/common/preprocessing/spectrogram/reshape.py
@@ -2,8 +2,10 @@ from typing import List
 import numpy as np
 
 
-def slice_spectrogram(spectrogram: np.ndarray, slice_size: int,
-                      stride_size: int) -> List[np.ndarray]:
+def slice_spectrogram(spectrogram: np.ndarray,
+                      slice_size: int,
+                      stride_size: int,
+                      copy: bool = False) -> List[np.ndarray]:
     """Slice a spectrogram.
 
     Args:
@@ -17,11 +19,14 @@ def slice_spectrogram(spectrogram: np.ndarray, slice_size: int,
     slices: List[np.ndarray] = list()
     for i in range(0, spectrogram.shape[1] - slice_size + 1, stride_size):
         curr_slice: np.ndarray = spectrogram[:, i:i + slice_size]
-        slices.append(curr_slice)
+        if copy == True:
+            slices.append(np.copy(curr_slice))
+        else:
+            slices.append(curr_slice)
     return slices
 
 
-def flatten_slice(slice: np.ndarray) -> np.ndarray:
+def flatten_slice(slice: np.ndarray, copy: bool = False) -> np.ndarray:
     """Convert a slice of spectrogram in to a row vector.
 
     Args:
@@ -34,4 +39,6 @@ def flatten_slice(slice: np.ndarray) -> np.ndarray:
     shingle_size: int = slice.shape[1]
     flat_slice: np.ndarray = slice.reshape((n_sample_freq * shingle_size),
                                            order="F")
+    if copy == True:
+        return np.copy(flat_slice)
     return flat_slice
