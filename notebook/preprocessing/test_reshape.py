c840ed4572b784f69211510afcbc2572716ce4ae
busFred <32878707+busFred@users.noreply.github.com>
2021-09-25 13:33:48 -0400

    add test_reshape.py

---
+++ notebook/preprocessing/test_reshape.py
@@ -0,0 +1,65 @@
+#%%
+from typing import List
+import librosa.core as rosa_core
+import numpy as np
+from audio_classifier.common.preprocessing.spectrogram import (reshape,
+                                                               transform)
+
+#%%
+AUDIO_PATH: str = "../test_audio/17592-5-1-1.wav"
+SAMPLE_RATE: int = 44100
+WINDOW_SIZE: int = 1024
+NFFT: int = 1024
+NMELS: int = 40
+
+#%%
+sound_wave, _ = rosa_core.load(AUDIO_PATH, sr=SAMPLE_RATE, mono=True)
+
+#%%[markdown]
+# # Generate mel-spectrogram with transform.transform_mel_spectrogram
+
+#%%
+mel_spec, mel_sample_freq, mel_sample_time = transform.transform_mel_spectrogram(
+    sound_wave=sound_wave,
+    sample_rate=SAMPLE_RATE,
+    n_fft=NFFT,
+    n_mels=NMELS,
+    freq_min=0,
+    freq_max=44100 / 2,
+    hop_size=WINDOW_SIZE,
+    window_size=WINDOW_SIZE,
+    apply_log=True)
+
+
+#%%
+def verify_slice(slice: np.ndarray, flat_slice: np.ndarray) -> bool:
+    for j in range(0, slice.shape[1]):  # iterate over the time axis
+        for i in range(0, slice.shape[0]):  # iterate over the freq axis
+            curr_idx = (j * slice.shape[0]) + i
+            if slice[i, j] != flat_slice[curr_idx]:
+                return False
+    return True
+
+
+#%% [markdown]
+# # Slice the spectrogram every 0.1 seconds.
+
+#%%
+spec_time_res: float = mel_sample_time[1] - mel_sample_time[0]
+SLICE_SIZE: int = int(0.1 / spec_time_res)
+STRIDE_SIZE: int = int(0.1 / spec_time_res)
+mel_spec_slices: List[np.ndarray] = reshape.slice_spectrogram(
+    spectrogram=mel_spec, slice_size=SLICE_SIZE, stride_size=STRIDE_SIZE)
+
+#%% [markdown]
+# ## result verified
+#%%
+is_succeed: bool = True
+for curr_slice in mel_spec_slices:
+    curr_flat_slice = reshape.flatten_slice(curr_slice)
+    is_succeed = is_succeed and verify_slice(curr_slice, curr_flat_slice)
+    if is_succeed == False:
+        print("failed")
+        break
+if is_succeed == True:
+    print("pass")
