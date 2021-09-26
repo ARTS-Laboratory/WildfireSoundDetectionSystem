268a00fdbb7a76aa7eaaca1719d3543ead9b2945
busFred <32878707+busFred@users.noreply.github.com>
2021-09-26 14:31:44 -0400

    remove type hint for collate.py

--- src/audio_classifier/train/dataset/utils/collate.py
+++ src/audio_classifier/train/dataset/utils/collate.py
@@ -1,7 +1,7 @@
 from typing import Any, List
 
 
-def identity_collate_function(data: List[List]) -> List[List[Any]]:
+def identity_collate_function(data):
     n_items: int = len(data[0])
     ret_data: List[List[Any]] = [list() for _ in range(0, n_items)]
     for data_point in data:
