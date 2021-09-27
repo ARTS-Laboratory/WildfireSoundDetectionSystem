3370777a4839334bc782819139e73d72410cd435
busFred <32878707+busFred@users.noreply.github.com>
2021-09-27 09:15:28 -0400

    ensemble collate function as a class

--- src/audio_classifier/train/collate/base.py
+++ src/audio_classifier/train/collate/base.py
@@ -18,20 +18,30 @@ def identity_collate_function(data: Union[List[Tuple], List]) -> List[List]:
     return ret_data
 
 
-def ensemble_collate_function(
-    data: List[Tuple],
-    collate_funcs: Sequence[Callable[[List[Tuple]],
-                                     List[Tuple]]]) -> List[List]:
-    """Transform the data with multiple collate function in `collate_funcs` and reshape with `identity_collate_function`.
+class EnsembleCollateFunction():
 
-    Args:
-        data (List[Tuple]): (n_batch, n_items) A list of tuple of items returned from upstream dataset.
-        collate_funcs (Sequence[Callable[[List[Tuple]], List[Tuple]]]): (n_collate_funcs, ) Each collate function should take a List[Tuple] of size (n_batch, n_items_input) and returns a List of size (n_batch, n_items_output).
+    __collate_funcs: Sequence[Callable[[List[Tuple]], List[Tuple]]]
 
-    Returns:
-        ret_data (List[List[Any]]): (n_items_output, n_batch)
-    """
-    for collate_func in collate_funcs:
-        data = collate_func(data)
-    ret_data = identity_collate_function(data)
-    return ret_data
+    def __init__(
+            self, collate_funcs: Sequence[Callable[[List[Tuple]],
+                                                   List[Tuple]]]) -> None:
+        """The constructor for EnsembleCollateFunction
+
+        Args:
+            collate_funcs (Sequence[Callable[[List[Tuple]], List[Tuple]]]): (n_collate_funcs, ) Each collate function should take a List[Tuple] of size (n_batch, n_items_input) and returns a List of size (n_batch, n_items_output).
+        """
+        self.__collate_funcs = collate_funcs
+
+    def __call__(self, data: List[Tuple]) -> List[List]:
+        """Transform the data with multiple collate function in `collate_funcs` and reshape with `identity_collate_function`.
+
+        Args:
+            data (List[Tuple]): (n_batch, n_items) A list of tuple of items returned from upstream dataset.
+
+        Returns:
+            ret_data (List[List[Any]]): (n_items_output, n_batch)
+        """
+        for collate_func in self.__collate_funcs:
+            data = collate_func(data)
+        ret_data = identity_collate_function(data)
+        return ret_data
