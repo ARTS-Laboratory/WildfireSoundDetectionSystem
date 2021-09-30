f4cdf4f430b078d6944dbf178fcc22b1a3df6b01
busFred <32878707+busFred@users.noreply.github.com>
2021-09-30 11:57:08 -0400

    add script package to setup_devel.py

--- setup_develop.py
+++ setup_develop.py
@@ -1,4 +1,4 @@
 import setuptools
 
-setuptools.setup(name="audio_classifier",
-                 package_dir={"": "src"})
+setuptools.setup(name="audio_classifier", package_dir={"": "src"})
+setuptools.setup(name="script", package_dir={"": "src"})
