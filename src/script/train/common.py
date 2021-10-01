631a10a6ca274bd9a24ce586e742306ba0e0d691
busFred <32878707+busFred@users.noreply.github.com>
2021-10-01 08:26:31 -0400

    update comments for common.py

--- src/script/train/common.py
+++ src/script/train/common.py
@@ -1,11 +1,14 @@
+from collections import deque
 from functools import partial
 from os import path
-from typing import Callable, Dict, Optional, Sequence, Tuple, Union
+from typing import Any, Callable, Deque, Dict, Optional, Sequence, Tuple, Union
 
 import audio_classifier.config.preprocessing.spec as conf_spec
 import audio_classifier.train.config.dataset as conf_dataset
+import audio_classifier.train.config.loader as conf_loader
 import audio_classifier.train.data.dataset.base as dataset_base
 import audio_classifier.train.data.dataset.composite as dataset_composite
+import audio_classifier.train.data.dataset.utils.batch as batch_utils
 import audio_classifier.train.data.metadata.query as metadata_query
 import audio_classifier.train.data.metadata.reader as metadata_reader
 from torch.utils.data import DataLoader
@@ -18,6 +21,14 @@ CollateFuncType = Callable[[Union[Sequence[Tuple], Sequence]],
 def get_metadata(
         dataset_config: conf_dataset.PreSplitFoldDatasetConfig
 ) -> MetaDataType:
+    """Read metadata from the path specified in dataset_config.
+
+    Args:
+        dataset_config (conf_dataset.PreSplitFoldDatasetConfig): The dataset_config to be used.
+
+    Returns:
+        MetaDataType: The loaded metadata.
+    """
     metadata = metadata_reader.read_csv_metadata(dataset_config.metadata_path)
     return metadata
 
@@ -27,6 +38,16 @@ def get_dataset_generator(
     dataset_config: conf_dataset.PreSplitFoldDatasetConfig,
     mel_spec_config: conf_spec.MelSpecConfig
 ) -> dataset_composite.KFoldDatasetGenerator:
+    """Create dataset generator.
+
+    Args:
+        metadata (MetaDataType): The metadata correspond the dataset.
+        dataset_config (conf_dataset.PreSplitFoldDatasetConfig): The dataset config to be used.
+        mel_spec_config (conf_spec.MelSpecConfig): The mel-spectrogram config to be used.
+
+    Returns:
+        dataset_composite.KFoldDatasetGenerator: The dataset generator.
+    """
     dataset_generator = dataset_composite.KFoldDatasetGenerator(
         k_folds=dataset_config.k_folds,
         sub_dataset_generator=partial(_sub_dataset_generator,
@@ -37,18 +58,36 @@ def get_dataset_generator(
 
 
 def generate_dataset(
-        curr_val_fold: int,
-        dataset_generator: dataset_composite.KFoldDatasetGenerator,
-        collate_function: CollateFuncType,
-        num_workers: int = 1):
+    curr_val_fold: int,
+    dataset_generator: dataset_composite.KFoldDatasetGenerator,
+    collate_function: CollateFuncType, loader_config: conf_loader.LoaderConfig
+) -> Sequence[Sequence[Sequence[Any]]]:
+    """Generate the train and validation dataset with the dataset_generator
+
+    Args:
+        curr_val_fold (int): The fold number to be used as validation dataset. Zero-indexed.
+        dataset_generator (dataset_composite.KFoldDatasetGenerator): The dataset generator to be used.
+        collate_function (CollateFuncType): The collate function used to process the time series data.
+        loader_config (conf_loader.LoaderConfig): The loader config used to load the dataset.
+
+    Returns:
+        Sequence[Sequence[Sequence[Any]]]: (2, n_items_output, n_data_points)
+    """
     datasets = dataset_generator.get_train_val_dataset(
         curr_val_fold=curr_val_fold)
+    ret_dataset: Deque[Sequence[Sequence[Any]]] = deque()
     for dataset in datasets:
         loader = DataLoader(dataset=dataset,
                             collate_fn=collate_function,
-                            num_workers=num_workers)
+                            num_workers=loader_config.num_workers,
+                            batch_size=loader_config.batch_size)
+        batches: Deque[Sequence[Sequence]] = deque()
         for batch in loader:
-            pass
+            batches.append(batch)
+        curr_ret_dataset: Sequence[
+            Sequence[Any]] = batch_utils.combine_batches(batches)
+        ret_dataset.append(curr_ret_dataset)
+    return ret_dataset
 
 
 def _sub_dataset_generator(
@@ -56,6 +95,17 @@ def _sub_dataset_generator(
         dataset_config: conf_dataset.PreSplitFoldDatasetConfig,
         spec_config: conf_spec.STFTSpecConfig,
         metadata: Optional[MetaDataType] = None) -> dataset_base.FolderDataset:
+    """The functor used to generate each sub dataset.
+
+    Args:
+        curr_fold (int): The current fold to be generated
+        dataset_config (conf_dataset.PreSplitFoldDatasetConfig): The dataset configuration to be used.
+        spec_config (conf_spec.STFTSpecConfig): The spectrogram configuration to be used.
+        metadata (Optional[MetaDataType], optional): The metadata used to map filename to class lable. If not provided, one will be loaded. Defaults to None.
+
+    Returns:
+        dataset_base.FolderDataset: The current fold dataset.
+    """
     folder_path: str = path.join(dataset_config.root_path,
                                  dataset_config.fold_path_stub)
     folder_path = str.format(folder_path, curr_fold + 1)
