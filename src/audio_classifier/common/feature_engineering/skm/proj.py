7a6e9ec28ad2759fa0ed0939ef65abcff14e8388
busFred <32878707+busFred@users.noreply.github.com>
2021-10-05 19:16:12 -0400

    update proj for class condition skm

--- src/audio_classifier/common/feature_engineering/skm/proj.py
+++ src/audio_classifier/common/feature_engineering/skm/proj.py
@@ -5,17 +5,21 @@ from sklearn_plugins.cluster.spherical_kmeans import SphericalKMeans
 
 
 def proj_skl_skm(spec_flat_slices: Sequence[np.ndarray],
-                 skm: SphericalKMeans,
-                 copy: bool = True) -> np.ndarray:
+                 skms: Sequence[SphericalKMeans]) -> np.ndarray:
     """Project the flat slices of an audio to trained cluster centroids.
 
     Args:
         audio_flat_slices (Sequence[np.ndarray]): (n_slices, n_mels * slice_size) The flat slices of a spectrogram.
-        skm (SphericalKMeans): The spherical k-means instance used to project incoming slices.
+        skm (Sequence[SphericalKMeans]): All the spherical k-means
 
     Returns:
-        spec_projs (np.ndarray): (n_slices, n_centroids) The projected slices.
+        spec_projs (np.ndarray): (n_slices, n_total_centroids) The projected slices.
     """
     slices: np.ndarray = np.asarray(spec_flat_slices)
-    spec_projs: np.ndarray = skm.predict(slices, copy)
+    spec_projs_list: List[np.ndarray] = [
+        skm.predict(slices, copy=True) for skm in skms
+    ]
+    if len(spec_projs_list) == 1:
+        return spec_projs_list[0]
+    spec_projs: np.ndarray = np.concatenate(tuple(spec_projs_list), axis=1)
     return spec_projs
