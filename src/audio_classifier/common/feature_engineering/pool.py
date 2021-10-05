6e9efd1467b1d0bb772393a559008629dfb99ba4
busFred <32878707+busFred@users.noreply.github.com>
2021-10-05 18:27:46 -0400

    update PoolFunc

--- src/audio_classifier/common/feature_engineering/pool.py
+++ src/audio_classifier/common/feature_engineering/pool.py
@@ -1,4 +1,5 @@
-from typing import Callable, Sequence, Tuple, Union
+from collections import deque
+from typing import Callable, Deque, Sequence, Tuple, Union, MutableSequence
 
 import numpy as np
 
@@ -8,12 +9,25 @@ class PoolFunc:
 
     def __init__(self, pool_funcs: Sequence[Callable[[np.ndarray],
                                                      np.ndarray]]):
+        """[summary]
+
+        Args:
+            pool_funcs (Sequence[Callable[[np.ndarray], np.ndarray]]): Each pooling function takes an np.ndarray of shape (n_slices, n_features) and output and np.ndarray of shape (n_output_features, )
+        """
         self.__pool_funcs = pool_funcs
 
     def __call__(self, input: np.ndarray) -> np.ndarray:
+        """[summary]
+
+        Args:
+            input (np.ndarray): (n_slices, n_features)
+
+        Returns:
+            output (np.ndarray): (n_output_features, )
+        """
         output_list: Tuple[np.ndarray, ...] = tuple(
             [pool_func(input) for pool_func in self.__pool_funcs])
-        output: np.ndarray = np.concatenate(output_list, axis=1)
+        output: np.ndarray = np.concatenate(output_list, axis=0)
         return output
 
 
@@ -22,7 +36,7 @@ def apply_pool_func(
     pool_func: Callable[[np.ndarray], np.ndarray],
     pool_size: int = -1,
     stride_size: int = -1,
-) -> np.ndarray:
+) -> Sequence[np.ndarray]:
     """Apply pooling function on the projection vectors of a file.
 
     Args:
@@ -32,21 +46,16 @@ def apply_pool_func(
         stride_size (int, optional): The stride of the sliding window. Defaults to -1 set the stride_size to pool_size.
 
     Returns:
-        pool_vectors (np.ndarray): (n_slices_prime, pool_func_out_dim)
+        pool_vectors (Sequence[np.ndarray]): (n_slices_prime, n_output_features)
     """
     if pool_size == -1:
         pool_size = len(spec_projs)
     if stride_size == -1:
         stride_size = pool_size
-    pool_vectors: Union[np.ndarray, None] = None
+    pool_projs: MutableSequence[np.ndarray] = deque()
     for begin_idx in range(0, len(spec_projs) - pool_size + 1, stride_size):
         end_idx: int = begin_idx + pool_size
-        curr_proj: np.ndarray = spec_projs[begin_idx:end_idx]
-        curr_pool: np.ndarray = pool_func(curr_proj)
-        if pool_vectors is not None:
-            pool_vectors = np.vstack((pool_vectors, curr_pool))
-        else:
-            pool_vectors = curr_pool
-    if pool_vectors is None:
-        raise ValueError("pool_vectors is None")
-    return pool_vectors
+        curr_spec_projs: np.ndarray = spec_projs[begin_idx:end_idx, :]
+        curr_pool_proj: np.ndarray = pool_func(curr_spec_projs)
+        pool_projs.append(curr_pool_proj)
+    return pool_projs
