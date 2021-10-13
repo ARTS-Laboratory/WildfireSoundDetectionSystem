041a61984de3523dc5eba9da157641b5c090413f
busFred <32878707+busFred@users.noreply.github.com>
2021-10-13 18:14:53 -0400

    add contrast collate

--- src/audio_classifier/train/collate/feature_engineering/contrast.py
+++ src/audio_classifier/train/collate/feature_engineering/contrast.py
@@ -1,33 +1,27 @@
 from collections import deque
-from typing import Callable, Deque, Sequence, Tuple
+from typing import Callable, Deque, List, Sequence, Tuple
 
 import numpy as np
 
-from ....common.feature_engineering import pool
-from ....config.feature_engineering.pool import PoolConfig
+from ....common.feature_engineering import contrast
+from ....config.feature_engineering.contrast import ContrastConfig
 
 
 def contrast_collate(
-    data: Sequence[Tuple[str, np.ndarray, np.ndarray, np.ndarray, int]],
-    pool_func: Callable[[np.ndarray], np.ndarray], pool_config: PoolConfig
+    data: Sequence[Tuple[str, Sequence[np.ndarray], np.ndarray, np.ndarray,
+                         int]], threshold_func: Callable[[np.ndarray], float],
+    contrast_config: ContrastConfig
 ) -> Sequence[Tuple[str, Sequence[np.ndarray], np.ndarray, np.ndarray, int]]:
-    """For a batch of data, apply pooling function to each of the projected audio slices.
-
-    Args:
-        data (Sequence[Tuple[str, np.ndarray, np.ndarray, np.ndarray, int]]): (batch_size, ) The data from upstream sklearn projection function.
-        pool_func (Callable[[np.ndarray], np.ndarray]): The pooling function to be applied to the incoming audio projections.
-
-    Returns:
-        ret_data (Sequence[Tuple[str, Sequence[np.ndarray], np.ndarray, np.ndarray, int]]): (batch_size, ) The transformed dataset with each data point being a tuple of (filename, pool_projs, sample_freq, sample_time, lable). pool_projs has size (n_slices_pr, n_output_features).
-    """
     ret_data: Deque[Tuple[str, Sequence[np.ndarray], np.ndarray, np.ndarray,
                           int]] = deque()
-    for filename, spec_projs, sample_freq, sample_time, label in data:
-        pool_projs: Sequence[np.ndarray] = pool.apply_pool_func(
-            spec_projs=spec_projs,
-            pool_func=pool_func,
-            pool_size=pool_config.pool_size,
-            stride_size=pool_config.stride_size)
+    for filename, spec_flat_slices, sample_freq, sample_time, label in data:
+        contrast_slices: List[np.ndarray] = [
+            contrast.apply_contrast_func(flat_slice=flat_slice,
+                                         threshold_func=threshold_func,
+                                         alpha=contrast_config.alpha,
+                                         beta=contrast_config.beta)
+            for flat_slice in spec_flat_slices
+        ]
         ret_data.append(
-            (filename, pool_projs, sample_freq, sample_time, label))
+            (filename, contrast_slices, sample_freq, sample_time, label))
     return ret_data
