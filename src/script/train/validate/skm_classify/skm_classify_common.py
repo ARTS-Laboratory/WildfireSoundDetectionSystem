3a972968fafcaa7a8a2711feb2b41a93c6a06208
busFred <32878707+busFred@users.noreply.github.com>
2021-11-08 12:06:33 -0500

    remove unused code

--- src/script/train/validate/skm_classify/skm_classify_common.py
+++ src/script/train/validate/skm_classify/skm_classify_common.py
@@ -26,7 +26,7 @@ def calculate_metrics(
     for spec_projs in dataset.all_file_spec_projs:
         curr_slices_preds: np.ndarray = classifier.predict(X=spec_projs)
         pred_label: int = np.argmax(np.bincount(curr_slices_preds,
-                                                minlength=2))[0]
+                                                minlength=2))
         pred_labels.append(pred_label)
     metrics = validate_common.ClassificationMetrics(
         accuracy=accuracy_score(y_true=dataset.labels, y_pred=pred_labels),
@@ -34,25 +34,3 @@ def calculate_metrics(
         confusion_matrix=confusion_matrix(y_true=dataset.labels,
                                           y_pred=pred_labels))
     return metrics
-    # slices_pred: np.ndarray = classifier.predict(dataset.all_file_spec_projs)
-    # filenames: List[str] = np.unique(dataset.filenames).tolist()
-    # pred_labels: List[int] = list()
-    # true_labels: List[int] = list()
-    # for curr_file in filenames:
-    #     # indices for current file
-    #     curr_indices: np.ndarray = np.argwhere(
-    #         dataset.filenames == curr_file).flatten()
-    #     # extract slice prediction for current file
-    #     curr_slices_pred: np.ndarray = slices_pred[curr_indices]
-    #     # vote for current file's label
-    #     pred_label: int = np.argmax(np.bincount(curr_slices_pred,
-    #                                             minlength=2))[0]
-    #     pred_labels.append(pred_label)
-    #     true_labels.append(dataset.labels[curr_indices[0]])
-    # roc_auc_score(y_true=true_labels, y_score=pred_labels)
-    # metrics = validate_common.ClassificationMetrics(
-    #     accuracy=accuracy_score(y_true=true_labels, y_pred=pred_labels),
-    #     roc_auc=roc_auc_score(y_true=true_labels, y_score=pred_labels),
-    #     confusion_matrix=confusion_matrix(y_true=true_labels,
-    #                                       y_pred=pred_labels))
-    # return metrics
