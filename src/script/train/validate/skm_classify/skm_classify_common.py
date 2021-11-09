248f8b6b259589538380da724603aa1024901cf8
busFred <32878707+busFred@users.noreply.github.com>
2021-11-09 18:32:47 -0500

    multiclass auc_roc curve

--- src/script/train/validate/skm_classify/skm_classify_common.py
+++ src/script/train/validate/skm_classify/skm_classify_common.py
@@ -23,14 +23,24 @@ def calculate_metrics(
         validate_common.ClassificationMetrics: The validation classification metrics.
     """
     pred_labels: Sequence[int] = list()
+    pred_probs: Sequence[np.ndarray] = list()
+    n_classes: int = max(dataset.labels) + 1
     for spec_projs in dataset.all_file_spec_projs:
-        curr_slices_preds: np.ndarray = classifier.predict(X=spec_projs)
-        pred_label: int = np.argmax(np.bincount(curr_slices_preds,
-                                                minlength=2))
+        curr_slices_preds: np.ndarray = classifier.predict(
+            X=np.asarray(spec_projs))
+        pred_label_count: np.ndarray = np.bincount(curr_slices_preds,
+                                                   minlength=n_classes)
+        pred_label: int = np.argmax(pred_label_count)
         pred_labels.append(pred_label)
+        if n_classes > 2:
+            pred_prob: np.ndarray = pred_label_count / np.sum(pred_label_count)
+            pred_probs.append(pred_prob)
     metrics = validate_common.ClassificationMetrics(
         accuracy=accuracy_score(y_true=dataset.labels, y_pred=pred_labels),
-        roc_auc=roc_auc_score(y_true=dataset.labels, y_score=pred_labels),
+        roc_auc=roc_auc_score(
+            y_true=dataset.labels,
+            y_score=pred_labels if n_classes == 2 else pred_probs,
+            multi_class="ovo"),
         confusion_matrix=confusion_matrix(y_true=dataset.labels,
                                           y_pred=pred_labels))
     return metrics
