f94f2bf9c8c981fb4129c11e2e1059a9d6dae2a1
busFred <32878707+busFred@users.noreply.github.com>
2021-12-13 18:24:08 -0500

    add proj_onnx_skm

--- src/audio_classifier/common/feature_engineering/skm_proj.py
+++ src/audio_classifier/common/feature_engineering/skm_proj.py
@@ -1,6 +1,7 @@
 from typing import List, Sequence
 
 import numpy as np
+from onnxruntime.capi.onnxruntime_inference_collection import InferenceSession
 from sklearn_plugins.cluster.spherical_kmeans import SphericalKMeans
 
 
@@ -18,6 +19,29 @@ def proj_skl_skm(spec_flat_slices: Sequence[np.ndarray],
     slices: np.ndarray = np.asarray(spec_flat_slices)
     spec_projs_list: List[np.ndarray] = [
         skm.transform(slices, copy=True) for skm in skms
+    ]
+    if len(spec_projs_list) == 1:
+        return spec_projs_list[0]
+    spec_projs: np.ndarray = np.concatenate(tuple(spec_projs_list), axis=1)
+    return spec_projs
+
+
+def proj_onnx_skm(spec_flat_slices: Sequence[np.ndarray],
+                  skms: Sequence[InferenceSession],
+                  dtype=np.float64) -> np.ndarray:
+    """Project the flat slices of an audio to trained cluster centroids.
+
+    Args:
+        audio_flat_slices (Sequence[np.ndarray]): (n_slices, n_mels * slice_size) The flat slices of a spectrogram.
+        skm (Sequence[SphericalKMeans]): All the spherical k-means
+
+    Returns:
+        spec_projs (np.ndarray): (n_slices, n_total_centroids) The projected slices.
+    """
+    slices: np.ndarray = np.asarray(spec_flat_slices, dtype)
+    spec_projs_list: List[np.ndarray] = [
+        skm.run(output_names=["scores"], input_feed={"X": slices})[0]
+        for skm in skms
     ]
     if len(spec_projs_list) == 1:
         return spec_projs_list[0]
