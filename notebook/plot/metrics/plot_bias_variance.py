5ea9b99e7494a7f12806689db4d2b83793915e94
busFred <32878707+busFred@users.noreply.github.com>
2021-12-19 15:10:10 -0500

    update plot_bias_variance

--- notebook/plot/metrics/plot_bias_variance.py
+++ notebook/plot/metrics/plot_bias_variance.py
@@ -1,52 +1,80 @@
 #%%
-import pickle
 import os
+import pickle
 from typing import List, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from script.validate.bias_variance import (BiasVarianceResult,
+                                           BiasVarianceResultBase)
 
 #%%
 plt.style.use(["science", "ieee"])
 
 #%%
-METRICS_DIR_PATH: str = "../../../metrics/binary_fire/spec_00_reshape_00_skm_00/bias_variance/svc_04"
+METRICS_DIR_PATH: str = "../../../metrics/binary_fire/spec_00_reshape_00_skm_00/bias_variance/svc_01"
 DATA_PER_FOLD: int = 588
 
 
 #%%
-def load_accs(accs_path: str):
-    with open(accs_path, mode="rb") as acc_file:
-        accs: Tuple[List[float], List[float],
-                    List[float]] = pickle.load(acc_file)
-        return accs
+def load_result(result_path: str):
+    with open(result_path, mode="rb") as result_file:
+        result: BiasVarianceResult = pickle.load(result_file)
+        return result
 
 
 #%%
+def plot_acc_vs_n_samples(result: BiasVarianceResultBase):
+    fig, ax = plt.subplots()
+    ax.plot(np.linspace(588, 588 * len(result.train_accs),
+                        len(result.train_accs)),
+            result.train_accs,
+            label="Train")
+    ax.plot(np.linspace(588, 588 * len(result.val_accs), len(result.val_accs)),
+            result.val_accs,
+            label="Validation")
+    ax.plot(np.linspace(588, 588 * len(result.test_accs),
+                        len(result.test_accs)),
+            result.test_accs,
+            label="Test")
+    ax.set_ylabel("Accuracy")
+    ax.set_xlabel("Number of Training Samples")
+    ax.legend(loc="center left", bbox_to_anchor=(1.04, 0.5))
+    fig.set_size_inches(fig.get_size_inches()[0] * 2, fig.get_size_inches()[1])
+    plt.tight_layout()
+    # plt.show()
+    return fig, ax
+
+
+def plot_elbow(result: BiasVarianceResult):
+    plots: List[Tuple[Figure, Axes]] = list()
+    n_clusters: np.ndarray = np.linspace(result.k_min, result.k_max,
+                                         (result.k_max - result.k_min) //
+                                         result.k_step)
+    for k_vals, k_scores in zip(result.k_vals, result.k_scores):
+        for curr_k_val, curr_k_scores in zip(k_vals, k_scores):
+            fig, ax = plt.subplots()
+            ax.plot(n_clusters, curr_k_scores)
+            ax.axvline(curr_k_val, linewidth=0.5, linestyle='--')
+            plots.append((fig, ax))
+    return plots
+
+
+#%%
+# plot accuracy
 filenames: List[str] = os.listdir(METRICS_DIR_PATH)
 filenames = list(
     filter(lambda fname: os.path.splitext(fname)[1] == ".pkl", filenames))
 for filename in filenames:
-    accs_path: str = os.path.join(METRICS_DIR_PATH, filename)
-    train_accs, val_accs, test_accs = load_accs(accs_path)
-    plt.plot(np.linspace(588, 588 * len(train_accs), len(train_accs)),
-             train_accs,
-             label="Train")
-    plt.plot(np.linspace(588, 588 * len(val_accs), len(val_accs)),
-             val_accs,
-             label="Validation")
-    plt.plot(np.linspace(588, 588 * len(test_accs), len(test_accs)),
-             test_accs,
-             label="Test")
-    plt.ylabel("Accuracy")
-    plt.xlabel("Number of Training Samples")
-    plt.legend(loc="center left", bbox_to_anchor=(1.04, 0.5))
-    cf = plt.gcf()
-    cf.set_size_inches(cf.get_size_inches()[0] * 2, cf.get_size_inches()[1])
-    plt.tight_layout()
-    plt.show()
-    fig_filename: str = os.path.splitext(filename)[0] + ".png"
+    result_path: str = os.path.join(METRICS_DIR_PATH, filename)
+    result = load_result(result_path)
+    # elbow plot
+    plots: List[Tuple[Figure, Axes]] = plot_elbow(result)
+    # accuracy vs number of data points
+    acc_fig, acc_ax = plot_acc_vs_n_samples(result)
+    fig_filename: str = "acc_" + os.path.splitext(filename)[0] + ".png"
     fig_path: str = os.path.join(METRICS_DIR_PATH, fig_filename)
-    plt.savefig(fig_path, dpi=300)
-
+    # acc_fig.savefig(fig_path, dpi=300)
 # %%
