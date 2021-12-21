3af3aded0f4cdef5d4750aa70f11e6b4fab14a97
busFred <32878707+busFred@users.noreply.github.com>
2021-12-21 10:39:25 -0500

    add fit_base_fix_k

---
+++ script/train/feature_engineering/skm/fit_base_fix_k.py
@@ -0,0 +1,123 @@
+import sys
+from argparse import ArgumentParser, Namespace
+from functools import partial
+from typing import List, Sequence, Union
+
+import audio_classifier.config.preprocessing.reshape as conf_reshape
+import audio_classifier.config.preprocessing.spec as conf_spec
+import audio_classifier.train.collate.base as collate_base
+import audio_classifier.train.collate.preprocessing.spectrogram.reshape as collate_reshape
+import audio_classifier.train.collate.preprocessing.spectrogram.transform as collate_transform
+import audio_classifier.train.config.alg as conf_alg
+import audio_classifier.train.config.dataset as conf_dataset
+import audio_classifier.train.config.loader as conf_loader
+import numpy as np
+from audio_classifier.train.data.dataset.composite import KFoldDatasetGenerator
+from script.train.feature_engineering.skm import fit_skm_base
+from script.train import train_common
+from script.train.feature_engineering import feature_common
+
+MetaDataType = train_common.MetaDataType
+CollateFuncType = train_common.CollateFuncType
+
+
+def main(args: List[str]):
+    argv: Namespace = parse_args(args)
+    export_path: str = argv.export_path
+    k_vals: Sequence[int] = argv.k_vals
+    dataset_config, mel_spec_config, reshape_config, skm_config, loader_config = get_config(
+        argv=argv)
+    metadata: MetaDataType = train_common.get_metadata(dataset_config)
+    dataset_generator: KFoldDatasetGenerator = train_common.get_dataset_generator(
+        metadata=metadata,
+        dataset_config=dataset_config,
+        mel_spec_config=mel_spec_config)
+    collate_func: CollateFuncType = collate_base.EnsembleCollateFunction(
+        collate_funcs=[
+            partial(collate_transform.mel_spectrogram_collate,
+                    config=mel_spec_config),
+            partial(collate_reshape.slice_flatten_collate,
+                    config=reshape_config)
+        ])
+    for curr_val_fold in range(dataset_config.k_folds):
+        train, _ = feature_common.generate_slice_dataset(
+            curr_val_fold=curr_val_fold,
+            dataset_generator=dataset_generator,
+            collate_function=collate_func,
+            loader_config=loader_config)
+        slices, labels = feature_common.convert_to_ndarray(slice_dataset=train)
+        unique_labels: np.ndarray = np.unique(labels)
+        for curr_label, k_value in zip(unique_labels, k_vals):
+            curr_class_path: str = fit_skm_base.get_curr_class_path(
+                export_path, curr_val_fold, curr_label)
+            curr_slices: np.ndarray = fit_skm_base.get_curr_class_slices(
+                curr_label, slices, labels)
+            skm, model_path = fit_skm_base.fit_skm(
+                curr_class_path=curr_class_path,
+                slices=curr_slices,
+                skm_config=skm_config,
+                k_value=k_value)
+            is_pass: bool = fit_skm_base.verify_model(slices=slices,
+                                                      skm=skm,
+                                                      model_path=model_path)
+            if is_pass == False:
+                print("Discrepency between src model and exported model",
+                      file=sys.stderr)
+                sys.exit(1)
+            fit_skm_base.plot_centroids(curr_plot_path=curr_class_path,
+                                        skm=skm,
+                                        spec_config=mel_spec_config,
+                                        reshape_config=reshape_config,
+                                        skm_config=skm_config,
+                                        sample_freq=train.sample_freqs[0],
+                                        sample_time=train.sample_times[0])
+            fit_skm_base.plot_silhouette(curr_plot_path=curr_class_path,
+                                         slices=curr_slices,
+                                         skm=skm,
+                                         k_value=k_value)
+
+
+def get_argparse() -> ArgumentParser:
+    parser = ArgumentParser(parents=[
+        conf_dataset.DatasetConfigArgumentParser(),
+        conf_spec.SpecConfigArgumentParser(),
+        conf_reshape.ReshapeConfigArgumentParser(),
+        conf_alg.SKMArgumentParser(),
+        conf_loader.LoaderConfigArgumentParser()
+    ])
+    parser.add_argument("--export_path",
+                        type=str,
+                        required=True,
+                        help="the export path")
+    parser.add_argument("--k_vals", type=int, nargs="*", required=True)
+    return parser
+
+
+def parse_args(args: List[str]) -> Namespace:
+    parser = get_argparse()
+    argv = parser.parse_args(args)
+    return argv
+
+
+def get_config(argv: Namespace):
+    DATASET_CONFIG_PATH: str = argv.dataset_config_path
+    SPEC_CONFIG_PATH: str = argv.spec_config_path
+    RESHAPE_CONFIG_PATH: str = argv.reshape_config_path
+    SKM_CONFIG_PATH: str = argv.skm_config_path
+    LOADER_CONFIG_PATH: str = argv.loader_config_path
+    dataset_config: conf_dataset.PreSplitFoldDatasetConfig = conf_dataset.get_dataset_config_from_json(
+        DATASET_CONFIG_PATH, argv, conf_dataset.PreSplitFoldDatasetConfig)
+    mel_spec_config: conf_spec.MelSpecConfig = conf_spec.get_spec_config_from_json(
+        SPEC_CONFIG_PATH, conf_spec.MelSpecConfig)
+    reshape_config: conf_reshape.ReshapeConfig = conf_reshape.get_reshape_config_from_json(
+        RESHAPE_CONFIG_PATH)
+    skm_config: conf_alg.SKMConfig = conf_alg.get_alg_config_from_json(
+        SKM_CONFIG_PATH, conf_alg.SKMConfig)
+    loader_config: conf_loader.LoaderConfig = conf_loader.get_loader_config_from_json(
+        LOADER_CONFIG_PATH)
+    return dataset_config, mel_spec_config, reshape_config, skm_config, loader_config
+
+
+if __name__ == "__main__":
+    args: List[str] = sys.argv[1:]
+    main(args)
